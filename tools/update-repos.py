72a668c60290b4ec5c71f64bc946c69976d56576
Ellie <el@horse64.org>
2023-09-28 04:07:09 +0200

    tools: Add new tool to help with repository housekeeping
    DCO-1.1-Signed-off-by: Ellie <el@horse64.org>

---
+++ tools/update-repos.py
@@ -0,0 +1,190 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import sys
+
+mydir = os.path.abspath(os.path.dirname(__file__))
+
+def are_files_different(p1, p2):
+    if not os.path.exists(p1) and os.path.exists(p2):
+        return True
+    if os.path.exists(p1) and not os.path.exists(p2):
+        return True
+    content1 = None
+    content2 = None
+    with open(p1, "r", encoding="utf-8") as f:
+        content1 = f.read()
+    with open(p2, "r", encoding="utf-8") as f:
+        content2 = f.read()
+    return (content1 != content2)
+
+if __name__ == "__main__":
+    # First, parse command line options:
+    dry_run = False
+    dco_signature = ""
+    args = sys.argv[1:]
+    index = -1
+    while (index + 1 < len(args)):
+        index += 1
+        arg = args[index]
+        if arg == "--dry-run":
+            dry_run = True
+            continue
+        if arg == "--help":
+            print("Helper tool to update all Horse64 repositories,\n"
+                "assuming sibling repositories are cloned into the\n"
+                "parent folder with unchanged folder names.\n"
+                "(If you don't know what any of this means, just\n"
+                "don't use it, it's meant for maintainer work.)")
+            sys.exit(0)
+        if arg in ["-v", "--version", "-V"]:
+            print("Version 2023.9")
+            sys.exit(0)
+        if arg == "--dco-signature" and index + 1 < len(args):
+            dco_signature = args[index + 1]
+            index += 1
+            continue
+        raise RuntimeError("Error: unknown argument: " + str(arg))
+    if (not "@" in dco_signature or
+            not dco_signature.endswith(">") or
+            not dco_signature.startswith("DCO-1.1-Signed-off-by") or
+            "@work.email" in dco_signature):
+        print("Error: Read LICENSE.md to understand and agree\n"
+            "to the DCO, then use --dco-signature to specify\n"
+            "your name and e-mail to sign the maintenance changes.\n"
+            " Example: --dco-signature "
+            "'DCO-1.1-Signed-off-by: Jane <jane@work.email>'")
+        raise RuntimeError("missing DCO info")
+
+    # Preparations and helper functions:
+    if dry_run:
+        print("(NOT changing anything, just a test run)")
+    def update_misc_years(rpath, to_year=2023):
+        contents = None
+        with open(os.path.join(rpath, "LICENSE.md"), "r",
+                encoding="utf-8") as f:
+            contents = f.read()
+        lines = contents.splitlines()
+        had_change = False
+        new_lines = []
+        for line in lines:
+            if (not line.startswith("Copyright (c) ") or
+                    not "ell1e" in line):
+                new_lines.append(line)
+                continue
+            remainder = line.partition("Copyright (c) ")[2].strip()
+            while len(remainder) > 0 and (
+                    (ord(remainder[0]) >= ord("0") and
+                    ord(remainder[0]) <= ord("9")) or
+                    remainder[0] == "-"):
+                remainder = remainder[1:]
+            first_year = line.partition("Copyright (c) ")[2].strip()
+            first_year = first_year.partition(" ")[0].strip()
+            if "-" in first_year:
+                first_year = first_year.partition("-")[0].strip()
+            if "," in first_year:
+                first_year = first_year.partition(",")[0]
+            first_year = int(first_year)
+            insert_year_str = str(first_year)
+            if first_year < to_year:
+                insert_year_str += "-" + str(to_year)
+            insert_line = ("Copyright (c) " + str(insert_year_str) +
+                remainder)
+            if line.strip().lower() == insert_line.strip().lower():
+                new_lines.append(line)
+                continue
+            new_lines.append(insert_line)
+            had_change = True
+        if dry_run and had_change:
+            if not dry_run:
+                with open(os.path.join(rpath, "LICENSE.md"), "w",
+                        encoding="utf-8") as f:
+                    f.write("\n".join(new_lines))
+                o = subprocess.check_output(["git", "add",
+                    "LICENSE.md"],
+                    cwd=rpath)
+            print("Updated " + os.path.basename(os.path.normpath(
+                rpath)) + ": LICENSE.md year bumped")
+            return True
+        return False
+
+    # Collect all repositories:
+    mainrepopath = os.path.join(mydir, "..")
+    repo_paths = [mainrepopath]
+    for reponame in os.listdir(os.path.join(mainrepopath, "..")):
+        if not reponame in ["hvm.horse64.org",
+                "hodoc.horse64.org",
+                "horp.horse64.org",
+                "horsels.horse64.org",
+                "hstyle.horse64.org",
+                "Spew3D",
+                "Spew3D-Web"]:
+            continue
+        print("Found repository: " + str(reponame))
+        repopath = os.path.join(mainrepopath, "..", reponame)
+        if not os.path.isdir(repopath):
+            continue
+        repo_paths.append(os.path.normpath(repopath))
+
+    # Make sure nothing is scheduled yet before we touch it:
+    for repopath in repo_paths:
+        print("Verifying repo is clean: " + str(repopath))
+        o = subprocess.check_output(["git", "status", "--porcelain=1"],
+            cwd=repopath).decode("utf-8")
+        lines = [l for l in o.splitlines()
+            if l.startswith("A")]
+        if len(lines) > 0:
+            print("Aborting, repository has changes scheduled: " +
+                repopath)
+            raise RuntimeError("found repository with changes to be "
+                "committed, deal with that first")
+
+    # Check all repositories if they need to be updated:
+    for repopath in repo_paths:
+        repopath = os.path.normpath(os.path.abspath(repopath))
+        reponame = os.path.basename(os.path.isdir(repopath))
+        print("Checking repository: " + str(reponame))
+        def update_rel_path_file(rel_path):
+            if (os.path.normpath(repopath) !=
+                    os.path.normpath(mainrepopath)):
+                return False
+            if type(rel_path) == list:
+                rel_path = os.path.join(*rel_path)
+            if are_files_different(
+                    os.path.join(repopath, rel_path),
+                    os.path.join(mainrepopath, rel_path)):
+                base_dir = (os.path.normpath(rel_path).
+                    rpartition(os.path.sep)[0])
+                if not dry_run:
+                    if not os.path.exists(base_dir):
+                        os.makedirs(base_dir)
+                    shutil.copyfile(os.path.join(mainrepopath, rel_path),
+                        os.path.join(repopath, rel_path))
+                    o = subprocess.check_output(["git", "add",
+                        rel_path],
+                        cwd=repopath)
+                print("Updated " + reponame + " by updating: " +
+                    rel_path + " (inside: " + str(base_dir) + ")")
+                return True
+            return False
+        if (update_rel_path_file(
+                    [".gitea", "ISSUE_TEMPLATE", "bug.yml"]) or
+                update_rel_path_file(
+                    [".gitea", "ISSUE_TEMPLATE", "docs.yml"]) or
+                update_rel_path_file(
+                    [".gitea", "ISSUE_TEMPLATE", "proposal.yml"]) or
+                update_rel_path_file(
+                    [".git", "hooks", "commit-msg"]) or
+                update_rel_path_file(
+                    [".github", "workflows",
+                        "close-all-pull-requests.yml"]) or
+                update_misc_years(repopath)):
+            if not dry_run:
+                o = subprocess.check_output(["git", "commit",
+                    rel_path, "-m", "housekeeping: Updating "
+                    "misc repository meta files via "
+                    "tools/update-repos.py",
+                    "-m", dco_signature],
+                    cwd=repopath)
+
